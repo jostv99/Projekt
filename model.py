57797f06859f3e8ed3bac40b0a229899bba5f641
jostv99 <49946976+jostv99@users.noreply.github.com>
2021-09-02 21:24:01 +0200

    Test

---
+++ model.py
@@ -0,0 +1,17 @@
+
+class Uporabnik:
+    def __init__(self, uporabnisko_ime, geslo):
+        self.uporabnisko_ime = uporabnisko_ime
+        self.geslo = geslo
+
+class Recept:
+    def __init__(self, jed):
+        self.jed = jed
+
+
+
+
+
+
+
+
