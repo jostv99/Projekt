d1f6cd35722a25107fb31ad7119f9cd7e2d6a9f9
jostv99 <49946976+jostv99@users.noreply.github.com>
2021-09-04 09:54:17 +0200

    Dodal osnovni html, spletni vmesnik

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import json
 
 class Uporabnik:
     def __init__(self, uporabnisko_ime, geslo):
@@ -7,9 +8,19 @@ class Uporabnik:
 class Recept:
     def __init__(self, jed):
         self.jed = jed
+        self.cas_priprave = None
+        self.cas_kuhanja = None
+        self.cas_skupni = None
+        self.sestavine = {}
 
+    def dodaj_sestavino(self, sestavina, kolicina, enota=None):
+            self.sestavine[sestavina] = (kolicina, enota)
 
-
+    def nastavi_cas(self, priprave, kuhanja):
+        #cas v min
+        self.cas_priprave = priprave
+        self.cas_kuhanja = kuhanja
+        self.cas_skupni = kuhanja + priprave
 
 
 
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,1 +1,27 @@
 import bottle
+
+
+@bottle.get("/")
+def osnovna_stran():
+    return bottle.template("osnovna_stran.html")
+
+@bottle.get("/prijava")
+def prijava_get():
+    return bottle.template("prijava.html")
+
+@bottle.post("/prijava")
+def prijava_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    geslo = bottle.request.forms.getunicode("geslo")
+    
+@bottle.get("/registracija")
+def registracija_get():
+    return bottle.template("registracija")
+
+@bottle.post("/registracija")
+def registracija_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    geslo = bottle.request.forms.getunicode("geslo")
+
+    
+bottle.run(debug=True, reloader=True)
